6e6dfd5e3ca80170c0362c0faf0e1834dda83685
midrekt <arenterwer@gmail.com>
2023-05-24 13:19:08 +0700

    plusi

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+            char vib;
+
+            Console.WriteLine("Введите число А::");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите число В::");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите наименование оператора:");
+            vib = Convert.ToChar(Console.ReadLine());
+
+
+            switch (vib)
+            {
+                case '+':
+                    Console.WriteLine(a + b);
+                    break;
+                case '-':
+
+                    break;
+                case '*':
+
+                    break;
+                case '/':
+
+                    break;
+                default:
+                    Console.WriteLine("Неизвестный оператор");
+                    break;
+            }
+        }
+    }
+}
